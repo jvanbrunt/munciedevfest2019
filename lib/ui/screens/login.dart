75627a9394f00f7af6e8f7635b7e39812c24e066
Jacob Van Brunt <jvanbrunt@developertown.com>
2019-09-13 21:24:18 -0400

    Adding background decorator.

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -4,9 +4,16 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Private methods within build method help us to
-    // organize our code and recognize structure of widget
-    // that we're building:
+    // New private method which includes the background image:
+    BoxDecoration _buildBackground() {
+      return BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/brooke-lark-385507-unsplash.jpg"),
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+
     Text _buildText() {
       return Text(
         'Recipes',
@@ -15,20 +22,21 @@ class LoginScreen extends StatelessWidget {
     }
 
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _buildText(),
-            // Space between "Recipes" and the button:
-            SizedBox(height: 50.0),
-            MaterialButton(
-              color: Colors.white,
-              child: Text("Sign In with Google"),
-              onPressed: () => print("Button pressed."),
-            )
-          ],
+      body: Container(
+        decoration: _buildBackground(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildText(),
+              SizedBox(height: 50.0),
+              MaterialButton(
+                color: Colors.white,
+                child: Text("Sign In with Google"),
+                onPressed: () => print("Button pressed."),
+              ),
+            ],
+          ),
         ),
       ),
     );
