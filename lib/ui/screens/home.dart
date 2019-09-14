ba8aa419d4920879b28ab92371313841e7a97b5d
Jacob Van Brunt <jvanbrunt@developertown.com>
2019-09-14 09:19:17 -0400

    Creating statefull homescreen.

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:muncie_dev_fests/model/recipe.dart';
+import 'package:muncie_dev_fests/utils/store.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  List<Recipe> recipes = getRecipes();
+  List<String> userFavorites = getFavoritesIDs();
+
+  void _handleFavoritesListChanged(String recipeID) {
+    setState(() {
+      if (userFavorites.contains(recipeID)) {
+        userFavorites.remove(recipeID);
+      } else {
+        userFavorites.add(recipeID);
+      }
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    double _iconSize = 20.0;
+    // New method:
+    Column _buildRecipes(List<Recipe> recipesList) {
+      return Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: recipesList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(recipesList[index].name),
+                );
+              },
+            ),
+          ),
+        ],
+      );
+    }
+
+    const double _iconSize = 20.0;
 
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: PreferredSize( // New code
+        appBar: PreferredSize(
           // We set Size equal to passed height (50.0) and infinite width:
-          preferredSize: Size.fromHeight(50.0), // New code
+          preferredSize: Size.fromHeight(50.0),
           child: AppBar(
             backgroundColor: Colors.white,
             elevation: 2.0,
@@ -28,11 +67,20 @@ class HomeScreen extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.all(5.0),
           child: TabBarView(
-            // Placeholders for content of the tabs:
+            // Replace placeholders:
             children: [
-              Center(child: Icon(Icons.restaurant)),
-              Center(child: Icon(Icons.local_drink)),
-              Center(child: Icon(Icons.favorite)),
+              // Display recipes of type food:
+              _buildRecipes(recipes
+                  .where((recipe) => recipe.type == RecipeType.food)
+                  .toList()),
+              // Display recipes of type drink:
+              _buildRecipes(recipes
+                  .where((recipe) => recipe.type == RecipeType.drink)
+                  .toList()),
+              // Display favorite recipes:
+              _buildRecipes(recipes
+                  .where((recipe) => userFavorites.contains(recipe.id))
+                  .toList()),
               Center(child: Icon(Icons.settings)),
             ],
           ),
